ab709ea7f73beff6e704330c2f1e717bb8fcebe2
toorneini <gol.toorneini@gmail.com>
2022-05-08 18:03:22 +0430

    fix: fixing images's size

--- src/components/card/card-component.js
+++ src/components/card/card-component.js
@@ -3,7 +3,7 @@ import './card-style.css';
 
 export const Card =  props => (
     <div className='card-container'>
-        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180*180`} />
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`} />
         <h2> { props.monster.name } </h2>
         <p> { props.monster.email } </p>
     </div>
